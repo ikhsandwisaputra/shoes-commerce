7bfa9e74bf83b6cd1a2cbfec2f8a3b0e00f0fc69
ikhsandwisaputra <ikhsandwisaputraaaa@gmail.com>
2025-07-14 19:28:20 +0700

    Fix url

--- src/components/Lenis.tsx
+++ src/components/Lenis.tsx
@@ -4,7 +4,7 @@ import Lenis from 'lenis';
 const useLenis = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // feel like iOS scroll
     });
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const IconMenu = () => <svg className="w-6 h-6" fill="none" stroke="currentColor
 const IconClose = () => <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>;
 
 const navLinks = [
-  { name: 'HOME', href: '/', },
+  { name: 'HOME', href: '/shoes-commerce/', },
   { name: 'COLLECTION', href: '#' },
   { name: 'FOR HIM', href: '#' },
   { name: 'FOR HER', href: '#' },
